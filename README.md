fbb8cd3ebe3ae1d1dba80853909d57404bc32f3d
Ross MacLean <ross@rossmac.co.uk>
2022-02-24 15:57:13 +0000

    Restructure and fix deploy.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MailReader
+# MailClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.4.
 
--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app = express(),
-  bodyParser = require('body-parser');
-const port = 3000;
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('/app/code/web'));
 app.use(bodyParser.json());
-app.use(express.static("/app/code/"));
 
 app.listen(port, () => {
   console.log(`Server listening on port::${port}`);
